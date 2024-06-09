8bf2010663780f88e6f6a6878a42b776df7aeda8
MichalZlotnik <michal.zlotnik@studium.uni-hamburg.de>
2024-06-09 19:34:42 +0200

    MZL ARIMA_Models Rewritten Upload, Dynamic Paths

--- .venv/Scripts/ARIMA_Model_v1.py
+++ .venv/Scripts/ARIMA_Model_v1.py
@@ -8,7 +8,8 @@ from statsmodels.tsa.stattools import adfuller
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from statsmodels.tsa.arima.model import ARIMAResults
 
-
+date_column_name = 'date'
+value_column_name = 'value'
 
 #Dynamic Directories
 if os.getenv('PYTHON_ENV') == 'pycharm':
@@ -16,19 +17,21 @@ if os.getenv('PYTHON_ENV') == 'pycharm':
 else:
     data_dir = '/content/ElectricityGermany/data'
 
-file_name = 'day_ahead_price_germany.csv'
+file_name = 'day_ahead_price_germany_0.csv'
 file_path = os.path.join(data_dir, file_name)
 print(f"Loading data from {file_path}")
+
+
 
 
 df = pd.read_csv(file_path)
 
 # Filterinng out prices that are negative or zero
 
-df = df[df['Day Ahead Auction Price (EUR/MWh)'] > 0]
+df = df[df[value_column_name] > 0]
 
 # Converting the date column to account for the time stamps in it
-df['Date (GMT+1)'] = pd.to_datetime(df['Date (GMT+1)'], utc=True)
+df[date_column_name] = pd.to_datetime(df[date_column_name], utc=True)
 
 
 # Getting basic information about the data
@@ -36,29 +39,29 @@ df.info()
 
 # Plotting the data
 plt.figure(figsize=(10, 5))
-plt.plot(df['Date (GMT+1)'], df['Day Ahead Auction Price (EUR/MWh)'])
+plt.plot(df[date_column_name], df[value_column_name])
 plt.title('Day Ahead Prices')
-plt.xlabel('Date (GMT+1)')
-plt.ylabel('Day Ahead Auction Price (EUR/MWh)')
+plt.xlabel(date_column_name)
+plt.ylabel(value_column_name)
 plt.grid(True)
 plt.show()
 
 # Converting the data into logarithmic output to stabilize the variance
-df['Day Ahead Auction Price (EUR/MWh)'] = np.log(df['Day Ahead Auction Price (EUR/MWh)']) # transform the data back when making real predictions
+df[value_column_name] = np.log(df[value_column_name]) # transform the data back when making real predictions
 
 
 #Plotting the data
 plt.figure(figsize=(10, 5))
-plt.plot(df['Date (GMT+1)'], df['Day Ahead Auction Price (EUR/MWh)'])
+plt.plot(df[date_column_name], df[value_column_name])
 plt.title('Day Ahead Prices')
-plt.xlabel('Date (GMT+1)')
-plt.ylabel('Day Ahead Auction Price (EUR/MWh)')
+plt.xlabel(date_column_name)
+plt.ylabel(value_column_name)
 plt.grid(True)
 plt.show()
 
 # Splitting the data into test and training set
 
-X = df[['Date (GMT+1)', 'Day Ahead Auction Price (EUR/MWh)']]
+X = df[[date_column_name, value_column_name]]
 
 train_idx, test_idx = train_test_split(df.index, test_size=0.2, random_state=42)
 print("Train set size: ", len(train_idx))
@@ -67,10 +70,10 @@ print("Test set size", len(test_idx))
 # Check for stationarity of time series which is a requirement of ARIMA Models
 
 
-data = df['Day Ahead Auction Price (EUR/MWh)']
+data = df[value_column_name]
 
-train_data = df.loc[train_idx]['Day Ahead Auction Price (EUR/MWh)']
-test_data = df.loc[test_idx]['Day Ahead Auction Price (EUR/MWh)']
+train_data = df.loc[train_idx][value_column_name]
+test_data = df.loc[test_idx][value_column_name]
 
 result_train = adfuller(train_data)
 
@@ -90,17 +93,17 @@ from statsmodels.tsa.arima.model import ARIMA
 train_data.reset_index(drop=True, inplace=True)
 
  # Fit the ARIMA model using train_data directly as a Series
-model = ARIMA(train_data, order=(1, 1, 2))
+model = ARIMA(train_data, order=(1, 1, 1))
 model_fit = model.fit()
 
 # Print the summary of the ARIMA model
 print(model_fit.summary())
 
 #Forecasting Electricity Prices
-n_periods = 2  # Number of future periods to forecast
+n_periods = 1  # Number of future periods to forecast
 
 # Generate future timestamps for the forecasted periods
-future_dates = pd.date_range(start='2023-04-18', periods=n_periods, freq='h')
+future_dates = pd.date_range(start='2023-04-18', periods=n_periods, freq='D')
 
 # Make predictions for the future time periods
 forecast = model_fit.get_forecast(steps=n_periods)
